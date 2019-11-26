9d9d10c4a5cc656a88f69c4d64b5ed3780976335
kamilmichna <kamil.michna28@gmail.com>
2019-11-26 20:19:55 +0100

    finished contact - fix

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/run/media/kamil/b7319a4f-ac8a-4e58-90d7-c6cf95693042/programowanie/webdev/kamilmichna.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/run/media/kamil/b7319a4f-ac8a-4e58-90d7-c6cf95693042/programowanie/webdev/kamilmichna.github.io/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/run/media/kamil/b7319a4f-ac8a-4e58-90d7-c6cf95693042/programowanie/webdev/kamilmichna.github.io/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/run/media/kamil/b7319a4f-ac8a-4e58-90d7-c6cf95693042/programowanie/webdev/kamilmichna.github.io/src/pages/page-2.js")))
