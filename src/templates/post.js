a8099f339495862f80b32a7e91a297fa18f689c3
Kamil Michna <kamil.michna28@gmail.com>
2020-02-04 17:10:54 +0100

    added basic styling

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,16 +5,10 @@ import Layout from '../components/layout';
 import Header from '../components/Header';
 const PostContainer = styled.main`
 display: flex;
-
 flex-direction:column;
 align-items: center;
-  grid-column: 1/13;
-  min-height: 93vh;
-  height: 100%;
-  background-color: #E9EBEE;
-  overflow-y: auto;
-  padding-top: 20vh;
-  padding-bottom: 10vw;
+grid-column: 1/13;
+height: auto !important;
 `
 const PostContent = styled.main`
 position: relative;
@@ -22,20 +16,37 @@ display: flex;
 flex-direction:column;
 background-color: white;
 width:80%;
-height:100vh;
+height:auto;
+margin-top: 20vh;
+margin-bottom: 20vh;
+box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 & pre{
-  position: absolute;
-  padding-left: 60px;
   max-width: 70%;
   left: 60px;
+  padding-left: 20px;
   border-left: 6px solid #0CB7FC;
+}
+& h2,h3{
+    font-size: 2em;
+    color: black;
+    font-weight: 800;
+    margin-bottom: 50px;
+    margin-top: 50px;
+  }
+
+& code{
+  padding: 2px;
+  background-color: lightgrey;
 }
 & p {
   text-indent: 20px;
-  padding: 30px;
   font-size: 1.5em;
+  padding-left: 20px;
+  padding-right: 20px;
   line-height: 1.5em;
-  color: rgba(0,0,0,0.87);
+  color: rgba(0,0,0,0.8);
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 
 `
@@ -44,6 +55,7 @@ height:100vh;
 const PostContentHeader = styled.div`
 display: flex;
 align-items: center;
+justify-content: space-between;
 width: 100%;
 top: -100px;
 height: 100px;
@@ -51,6 +63,10 @@ position: absolute;
 background: linear-gradient(rgb(12, 183, 252) 0%, rgb(26, 6, 255) 99.99%, rgba(12, 194, 252, 0) 100%);
 padding-left: 30px;
 box-sizing: border-box;
+  & p {
+    font-size: 2em;
+    color: white;
+  }
   & h2{
     font-size: 3em;
     color: white;
@@ -66,6 +82,7 @@ export default ({ data }) => {
         <PostContent>
           <PostContentHeader>
             <h2 dangerouslySetInnerHTML={{ __html: post.title }}></h2>
+            <p  dangerouslySetInnerHTML={{ __html: post.date }}></p>
           </PostContentHeader>
           <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
         </PostContent>
